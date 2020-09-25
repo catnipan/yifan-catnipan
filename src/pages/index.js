d14c930203e77f3487548cd1c051fd5c00c77511
Yifan Pan <4anpan@gmail.com>
2020-09-24 19:30:05 -0700

    init test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Yifan Pan</h1>
+    <section>Education</section>
+    <section>Work Experience</section>
+    <section>Project</section>
+  </div>
 }
