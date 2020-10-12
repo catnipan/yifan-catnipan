ae92ec835357db03ead529b06a83a235ad8fcc1b
Yifan Pan <4anpan@gmail.com>
2020-10-11 21:01:41 -0700

    v1

---
+++ src/components/social-account.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import LogoGithub from '../../static/logo_github.svg';
+import LogoLinkedin from '../../static/logo_linkedin.svg';
+import LogoMail from '../../static/logo_mail.svg';
+import LogoGithubActive from '../../static/logo_github_active.svg';
+import LogoLinkedinActive from '../../static/logo_linkedin_active.svg';
+import LogoMailActive from '../../static/logo_mail_active.svg';
+import LogoResume from '../../static/logo_resume.svg';
+import LogoResumeActive from '../../static/logo_resume_active.svg';
+import style from './social-account.module.css';
+
+function IconLink({ src, activeSrc, href }) {
+  const [icon, updateIcon] = useState(src);
+  return <a className={style.linkButton} href={href} target="_blank" rel="noreferrer">
+    <img
+      src={icon}
+      onMouseOver={() => updateIcon(activeSrc)}
+      onMouseOut={() => updateIcon(src)}
+    />
+  </a>
+}
+
+export default function() {
+  return <div>
+    <IconLink
+      src={LogoGithub}
+      activeSrc={LogoGithubActive}
+      href="https://github.com/catnipan"
+    />
+    <IconLink
+      src={LogoLinkedin}
+      activeSrc={LogoLinkedinActive}
+      href="https://linkedin.com/in/yifanpan/"
+    />
+    <IconLink
+      src={LogoMail}
+      activeSrc={LogoMailActive}
+      href="mailto:pan.yifa@northeastern.edu"
+    />
+    <IconLink
+      src={LogoResume}
+      activeSrc={LogoResumeActive}
+      href="https://www.dropbox.com/s/niwc26ly3gsiifc/Yifan_Pan_resume.pdf"
+    />
+  </div>
+}
