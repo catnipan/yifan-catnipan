164ada5d3ba5d7da26e21c2d4dfc0cd14fa7ad23
Yifan Pan <4anpan@gmail.com>
2020-10-21 01:03:50 -0700

    roll back

--- src/components/social-account.js
+++ src/components/social-account.js
@@ -45,7 +45,7 @@ export default function SocialAccount() {
       src={LogoResume}
       activeSrc={LogoResumeActive}
       alt="Resume"
-      href="/resume"
+      href={`/resume.pdf?v=${Math.random()}`}
     />
   </Fragment>
 }
